346ebc56e5646d2ec4c001dcfabea99bcb8f23e8
Diego Zito <diegozito.m@gmail.com>
2023-01-17 15:35:45 +0100

    class - js - functions - class excercises done

---
+++ section-03-js/10-array-methods.js
@@ -0,0 +1,5 @@
+// Array Methods
+// En JavaScript, un array es una estructura de datos que contiene una lista de elementos que almacenan múltiples valores en una sola variable. La fuerza de los js arrays reside en los métodos de los arrays. Los Array methodss o métodos de matrices son funciones incorporadas a JavaScript que podemos aplicar a nuestros arrays - Cada método tiene una función única que realiza un cambio o cálculo a nuestro array y nos ahorra escribir funciones comunes desde cero.
+
+// JS Array Methods
+// Para facilitar la manipulación de arrays en JavaScript, debemos utilizar métodos de arrays para facilitar nuestro trabajo y hacer el código más limpio.
