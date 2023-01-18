bfb278cee4646220e2f6a0f3c1f9959cfbd9dec6
Diego Zito <diegozito.m@gmail.com>
2023-01-18 17:11:14 +0100

    lab solutions addeds

---
+++ labs/JS/basic-algorithms/basic-algorithms-finished/js/index.js
@@ -0,0 +1,16 @@
+// Iteration 1: Names and Input
+
+
+// Iteration 2: Conditionals
+
+// Iteration 3: Loops
+// Iteration 3.1 'Name' to 'N A M E'
+
+// Iteration 3.2 'Name' to 'emaN'
+
+// Iteration 3.3 Lexicographic order
+
+
+// Bonus 1: Palindromes
+
+// Bonus 2: Lorem ipsum counter
